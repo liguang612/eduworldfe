b091c0d62b8a6a441ebd78aca99afc6d6ca7132a
Ragnie <p.lyquangk.lythedan@gmail.com>
2025-06-04 22:43:27 +0700

    config: deploy homepage

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-export const baseURL = 'http://localhost:8080';
-// export const baseURL = 'https://eduworldbe-production-2272.up.railway.app';
+// export const baseURL = 'http://localhost:8080';
+export const baseURL = 'https://eduworldbe-production-2272.up.railway.app';
 
 const axiosInstance = axios.create({
   baseURL,
